50d3dae3ddd60d9adfde8eeaca371812eb244a17
Paulo Fernando Maggio <paullomagio@gmail.com>
2026-04-15 15:09:44 -0300

    feat: estrutura inicial spring boot jpa

---
+++ src/test/java/javafx_springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package javafx_springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
